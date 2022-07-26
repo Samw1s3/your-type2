a59def4306c31dcd1c484669bb7dd6f62b8c73c3
Sam Knopper <samanthaknopper@hotmail.com>
2022-07-26 20:11:16 +0800

    async handler added

--- backend/controllers/meetupController.js
+++ backend/controllers/meetupController.js
@@ -1,27 +1,27 @@
-
+const asyncHandler = require('express-async-handler')
 // get meetups request GET /api/meetups
-const getMeetups = (req,res) => {
+const getMeetups = asyncHandler(async (req,res) => {
     res.status(200).json({message: 'Get Meetup'})
-}
+})
 
 // Create meetups request post /api/meetups
-const createMeetups = (req,res) => {
+const createMeetups = asyncHandler(async (req,res) => {
     if(!req.body.text) {
         res.status(400)
         throw new Error('Please add a text field')
     }
     res.status(200).json({message: 'Create Meetup'})
-}
+})
 
 // update meetups request put /api/meetups/:id
-const updateMeetup = (req,res) => {
+const updateMeetup = asyncHandler(async (req,res) => {
     res.status(200).json({message: `update Meetup ${req.params.id}`})
-}
+})
 
 // delete meetups request put /api/meetups/:id
-const deleteMeetup = (req,res) => {
+const deleteMeetup = asyncHandler(async (req,res) => {
     res.status(200).json({message: `delete Meetup ${req.params.id}`})
-}
+})
 
 module.exports = {
   getMeetups, createMeetups, updateMeetup, deleteMeetup
